534e8646aab583c429047733970a680f0be3c942
Karim Ehab <karim.ehab.ai@gmail.com>
2026-09-07 02:40:01 +0300

    Finish Lab-1: Create a generative AI chat app

---
+++ labs/chat-app/chat-app.py
@@ -0,0 +1,103 @@
+import os
+from dotenv import load_dotenv
+
+# import namespaces
+from openai import OpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+
+
+def main(): 
+    # Clear the console
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    try:
+        # Get configuration settings 
+        load_dotenv()
+        azure_openai_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
+        model_deployment = os.getenv("MODEL_DEPLOYMENT")
+
+        # Initialize the OpenAI client
+        token_provider = get_bearer_token_provider(
+            DefaultAzureCredential(), "https://ai.azure.com/.default"
+        )
+
+        openai_client = OpenAI(
+            base_url=azure_openai_endpoint,
+            api_key=token_provider
+        )
+
+        # Track responses
+        last_response_id = None
+
+        # Loop until the user wants to quit
+        while True:
+            input_text = input('\nEnter a prompt (or type "quit" to exit): ')
+            if input_text.lower() == "quit":
+                break
+            if len(input_text) == 0:
+                print("Please enter a prompt.")
+                continue
+
+            #* Get a response
+            # To call LLMs we have 2 methods:
+            # 1- openai_client.chat.completions.create
+            # 2- openai_client.responses.create -> More simpler
+
+            #* Note: In (openai_client.responses.create), we have a parameter called 'stream',
+            #* and this returns a stream object.
+
+            #! Method 1
+            '''
+            completion = openai_client.chat.completions.create(
+                model=model_deployment,
+                messages=[
+                    {
+                        "role": "system",
+                        "content": "You are a helpful AI assistant that answers questions and provides information."
+                    },
+                    {
+                        "role": "user",
+                        "content": input_text
+                    }
+                ]
+            )
+            print(completion.choices[0].message.content)
+            '''
+
+            #! Method 2
+            '''
+            response = openai_client.responses.create(
+                        model=model_deployment,
+                        instructions="You are a helpful AI assistant that answers questions and provides information.",
+                        input=input_text,
+                        previous_response_id=last_response_id
+            )
+            print(response.output_text)
+            last_response_id=response.id
+            '''
+
+            #! Using streaming property
+            '''
+            stream = openai_client.responses.create(
+                        model=model_deployment,
+                        instructions="You are a helpful AI assistant that answers questions and provides information.",
+                        input=input_text,
+                        previous_response_id=last_response_id,
+                        stream=True # This is what enables the streaming (return a stream of events)
+            )
+
+            for event in stream:
+                if event.type == "response.output_text.delta":
+                    print(event.delta, end="")
+                elif event.type == "response.completed":
+                    last_response_id = event.response.id
+            print()
+            '''
+
+
+
+    except Exception as ex:
+        print(ex)
+
+if __name__ == '__main__': 
+    main()
